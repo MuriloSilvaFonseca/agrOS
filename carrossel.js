69ae306d36e70c5d3f416566ddc41522a44971a9
GuilhermeRGomes <guilhermerussigomes@gmail.com>
2025-10-22 17:37:51 -0300

    safwffwffwwf

---
+++ carrossel.js
@@ -0,0 +1,24 @@
+document.addEventListener('DOMContentLoaded', () => {
+let botoes = document.querySelectorAll('.btn-carrossel');
+
+botoes.forEach(botao => {
+    botao.addEventListener('click',() => {
+
+        botoes.forEach(b => b.classList.remove('ativo'));
+
+        botao.classList.add('ativo');
+    })
+})
+})
+function propriedade(){
+            document.getElementById("imagem").src="./imagem/dash.jpeg";
+}
+ function clientes(){
+            document.getElementById("imagem").src="./imagem/1.jpg";
+}
+ function servicos(){
+            document.getElementById("imagem").src="./imagem/dash.jpeg";
+}
+function equipamento(){
+            document.getElementById("imagem").src="./imagem/1.jpg";
+}
